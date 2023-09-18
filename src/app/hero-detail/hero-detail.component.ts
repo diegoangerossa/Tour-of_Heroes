fee8712131cdd6d298bd2dcdb5548d92896b89aa
Diego <diego.angerosa@spectec.net>
2023-09-18 16:50:51 -0300

    navigation

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Hero } from '../hero';
+import { HeroService } from '../services/hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+})
+export class HeroDetailComponent implements OnInit {
+
+  hero!: Hero;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private location: Location,
+    private heroService: HeroService
+    ) {}
+
+  ngOnInit(){
+    const heroId = Number(this.route.snapshot.paramMap.get("id"));
+    this.heroService.getHero(heroId)
+    .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(){
+    this.location.back();
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
-  heroes = HEROES;
-  selectedHero?: Hero;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  ngOnInit() {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
